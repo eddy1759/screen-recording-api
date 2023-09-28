b63a539fc9a75ef94edc83e8e2732af70c82f633
Edet Asuquo <eddy1759@gmail.com>
2023-09-28 15:01:04 +0100

    chore(init): setting up the project

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const httpStatus = require('http-status');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const compression = require('compression');
+var logger = require('morgan');
+var createError = require('http-errors');
+const { errorConverter, errorHandler } = require('./middleware/error');
+const ApiError = require('../utils/ApiError');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set security HTTP headers
+app.use(helmet());
+
+// sanitize request data
+app.use(xss());
+
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// limit repeated failed requests to auth endpoints
+if (config.env === 'production') {
+	app.use('/auth', authLimiter);
+}
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+	next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500).end();
+});
+
+module.exports = app;
